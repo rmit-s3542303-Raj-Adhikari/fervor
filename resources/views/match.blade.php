adfded9606ad4778966c2c92c43f00ab9194ba72
rmit-s3542303-raj-adhikari <peez@localhost.localdomain>
2017-08-15 16:09:35 +1000

    init

---
+++ resources/views/match.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title', ' - Matches')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><p>Matches</p></div>
+                <div class="panel-body">
+                    You have no matches
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title', ' - Profile')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading"><p>Profile</p></div>
+                <div class="panel-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
