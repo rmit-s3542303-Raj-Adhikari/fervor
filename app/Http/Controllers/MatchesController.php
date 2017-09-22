a6ed368e893ed0218731040b0e9458ebb9f8d33c
Utsav <s3542303@student.rmit.edu.au>
2017-09-22 18:04:55 +1000

    Added event cooldown + Debug Messages

--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -6,24 +6,37 @@ use App\Events\MatchRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Match;
 
 class MatchesController extends Controller
 {
     public function viewMatches () {
 
+        $user = User::find(Auth::id());
+
         $date = new \DateTime();
 
         $timestamp = $date->getTimestamp();
 
-        $lastreq = Auth::user()['lastMatchRequest'];
+        $lastreq = $user->lastMatchRequest;
 
         $difference = ($timestamp - $lastreq) / 60;
 
+        $debugmessage = array();
+        $debugmessage[] = 'Last request was at '.$lastreq;
+        $debugmessage[] = 'Last update was '.$difference.' minutes ago';
 
         if ($difference > 5){
-            event(new MatchRequest(User::find(Auth::id())));
+            $user->lastMatchRequest = $timestamp;
+            $user->save();
+            event(new MatchRequest($user));
+            $debugmessage[] = 'Dispatched Match Request';
         }
 
-        return ('matches');
+        $matches = Match::where('user', '=', $user->id)
+            ->where('score', '>', 30)->get();
+
+        return (view('match', ['debug' => $debugmessage, 'matches'=> $matches]));
     }
+
 }
