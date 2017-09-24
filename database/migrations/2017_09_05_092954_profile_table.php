b3525bdab0e541dd9137216c66a66fd76f1c7d13
Utsav <s3542303@student.rmit.edu.au>
2017-09-24 12:47:59 +1000

    Linted

--- database/migrations/2017_09_05_092954_profile_table.php
+++ database/migrations/2017_09_05_092954_profile_table.php
@@ -13,15 +13,11 @@ class ProfileTable extends Migration
      */
     public function up()
     {
-        Schema::create('profiles', function(Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
 
             $table->integer('user_id');
             $table->string('nickname')->nullable();
             $table->string('location')->nullable();
-
-
-
-
 
             $table->string('status')->nullable();
             $table->string('occupation')->nullable();
@@ -29,19 +25,13 @@ class ProfileTable extends Migration
 
             $table->boolean('smoking')->nullable();
 
-
-
-
-
             $table->string('religion')->nullable();
-
 
             $table->text('language')->nullable();
             $table->text('language2')->nullable();
             $table->text('language3')->nullable();
             $table->text('language4')->nullable();
             $table->text('language5')->nullable();
-
 
 
             $table->string('Ethnicity')->nullable();
@@ -55,37 +45,21 @@ class ProfileTable extends Migration
             $table->string('interest5')->nullable();
 
 
-
             $table->string('hobbies')->nullable();
             $table->string('hobbies2')->nullable();
             $table->string('hobbies3')->nullable();
             $table->string('hobbies4')->nullable();
             $table->string('hobbies5')->nullable();
 
+            $table->timestamps();
+        });
+    }
 
-
-
-
-            /*
-                        $table->string('genderPreference')->nullable();
-                        $table->integer('agePreference')->nullable();
-
-                        $table->string('cityPreference')->nullable();
-                        $table->string('religionPreference')->nullable();
-                        $table->string('ethnicityPreference')->nullable();
-                        $table->string('interestPreference')->nullable();
-                        $table->string('hobbiesPreference')->nullable();
-
-           */
-                        $table->timestamps();
-                    });
-                }
-
-                /**
-                 * Reverse the migrations.
-                 *
-                 * @return void
-                 */
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('profiles');
