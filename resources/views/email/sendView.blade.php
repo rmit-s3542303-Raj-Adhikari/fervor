7714f9879e9765f430bdfcec9f2de1a57be1e052
Ryan <s3489435@student.rmit.edu.au>
2017-09-24 04:32:28 +0000

    Seeding Files created. Profile Picture upload completed

--- resources/views/email/sendView.blade.php
+++ resources/views/email/sendView.blade.php
@@ -1,11 +1,7 @@
 <img alt="Fervor" src="/img/logo-white.png" class="nav-logo">
-<<<<<<< HEAD
+
 
   <h2>Verify Your Email Address</h2>
-
-=======
-Hello 
->>>>>>> Upload profile image functionality
 
 <p>Thank you for joining the Fervor network.
 Please click the link below to validate your email address and activate your account.</p>
