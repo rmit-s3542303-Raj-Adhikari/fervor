4967e5560639c197212ef025c41394c2d70838b3
Utsav <s3542303@student.rmit.edu.au>
2017-09-24 19:11:29 +1000

    Added missing import

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Profile;
 use App\User;
+use App\Preferences;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
