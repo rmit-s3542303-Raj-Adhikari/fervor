592521ac2e2a5391ad4fddf89e25b648eccb2cab
Utsav <s3542303@student.rmit.edu.au>
2017-09-24 19:12:41 +1000

    Added preferences model

--- app/Preferences.php
+++ app/Preferences.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Preferences extends Model
 {
     protected $fillable = [
-        'firstname', 'lastname', 'firstLogin', 'dob' ,'email', 'password', 'gender', 'preference','verifytoken'
+        'id','firstname', 'lastname', 'firstLogin', 'dob' ,'email', 'password', 'gender', 'preference','verifytoken'
     ];
 
     public $incrementing = false;
