fdd5ccf355540bfd24586592a38fa02f588b15c5
rmit-s3465920-labib-ali <s3465920@student.rmit.edu.au>
2017-09-26 07:54:49 +1000

    preference ethnicity controller updated

--- app/Http/Controllers/preferenceController.php
+++ app/Http/Controllers/preferenceController.php
@@ -8,8 +8,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Preferences;
+use Illuminate\Http\Request;
 
-class preferenceController
+use Auth;
+
+
+
+class preferenceController extends Controller
 {
 
 
@@ -17,6 +23,65 @@ class preferenceController
 
         return view('preference');
     }
+
+
+
+
+
+    public function updatePreference(Request $request){
+
+
+
+
+
+        $ethnCaucasian = $request->get('caucasian');
+        $ethnHispanic = $request->get('hispanic');
+        $ethBlack = $request->get('black');
+
+        $ethMiddleeast = $request->get('middleeast');
+        $ethAsian = $request->get('asian');
+        $ethIndian = $request->get('indian');
+        $ethAboriginal = $request->get('aboriginal');
+        $ethIslander = $request->get('islander');
+        $ethMixed = $request->get('mixed');
+
+            $smokingcheck  = $request->get('smoking');
+
+            if($smokingcheck === 'TRUE'){
+
+                $smoking = TRUE;
+
+            }else{
+
+                $smoking = FALSE;
+
+            }
+
+
+            $profile = Preferences::where('id', '=', 1 );
+
+            $profile->update([
+
+
+
+                'smoking'   => $smoking,
+                'caucasian' => $ethnCaucasian,
+                'hispanic'  => $ethnHispanic,
+                'black'     => $ethBlack,
+                'middleeast'     => $ethMiddleeast,
+                'asian'     => $ethAsian,
+                'indian'     => $ethIndian,
+                'aboriginal'     => $ethAboriginal,
+                'islander'     => $ethIslander,
+                'mixed'       => $ethMixed,
+
+
+            ]);
+
+
+            return redirect()->route('preference');
+        }
+
 
 
 }
