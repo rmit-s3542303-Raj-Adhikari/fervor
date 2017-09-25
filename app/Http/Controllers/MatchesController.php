4cdd12475a27ac1ab34c71b3f434934cadd4dbed
rmit-s3542303-Raj-Adhikari <s3542303@student.rmit.edu.au>
2017-09-26 00:16:49 +1000

    Properly filter matches before passing to view

--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -6,10 +6,14 @@ use App\Events\MatchRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use App\Match;
 
 class MatchesController extends Controller
 {
+
+    public static $SCORE_THRESHOLD = 0;
+
     public function viewMatches () {
 
         $user = User::find(Auth::id());
@@ -26,17 +30,33 @@ class MatchesController extends Controller
         $debugmessage[] = 'Last request was at '.$lastreq;
         $debugmessage[] = 'Last update was '.$difference.' minutes ago';
 
-        if ($difference > 5){
+        if ($difference > 1){
             $user->lastMatchRequest = $timestamp;
             $user->save();
+            
             event(new MatchRequest($user));
             $debugmessage[] = 'Dispatched Match Request';
         }
 
         $matches = Match::where('user', '=', $user->id)
-            ->where('score', '>', 30)->get();
+            ->where('score', '>=', MatchesController::$SCORE_THRESHOLD)->get();
 
-        return (view('match', ['debug' => $debugmessage, 'matches'=> $matches]));
+        $filtered = [];
+
+        // filter the matches that score lower then the allowed threshold
+        foreach ($matches as $match){
+            // Retrieve the opposie match
+            $oppositeMatch = Match::where('user', '=', $match->prospect)
+            ->where('prospect', '=', $user->id)->first();
+            
+            // filter them
+            if($oppositeMatch->score >= MatchesController::$SCORE_THRESHOLD)
+            {
+                $filtered[] = $match;
+            }
+        }
+
+        return (view('match', ['debug' => $debugmessage, 'matches'=> $filtered]));
     }
 
 }
