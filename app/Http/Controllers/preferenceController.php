d0677173db0024f596c16587d04258cbd05a208d
rmit-s3465920-labib-ali <s3465920@student.rmit.edu.au>
2017-09-25 13:53:43 +1000

    Preference controller

---
+++ app/Http/Controllers/preferenceController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Labib
+ * Date: 25/9/17
+ * Time: 1:48 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class preferenceController
+{
+
+
+    public function preference(){
+
+        return view('preference');
+    }
+
+
+}
