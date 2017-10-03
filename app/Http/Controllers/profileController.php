1f93455f6a7186f6510fe323848938880d466823
rmit-s3465920-labib-ali <s3465920@student.rmit.edu.au>
2017-10-03 11:10:47 +1100

    app Navigation bar updated

--- app/Http/Controllers/profileController.php
+++ app/Http/Controllers/profileController.php
@@ -25,10 +25,7 @@ class profileController extends Controller
 
 
 
-    public function profile(){
-        
-        return view('profile');
-    }
+
 
 
     public function addProfile(Request $request){
