a14d618edca585539a01879f7036ef8d2d65aa5a
rmit-s3542303-Raj-Adhikari <s3542303@student.rmit.edu.au>
2017-10-21 01:50:54 +1100

    Implemented messaging between users
    Added Message form
    
    Added message styling and display
    
    Added UI prompts
    
    Made Pages Responsive

---
+++ resources/views/layouts/message-rec.blade.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-md-6 col-lg-5 col-sm-7 message">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+            {{$post->body}}
+        </div>
+    </div>
+</div>
+</div>
