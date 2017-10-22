a433c6c2f1ca82dab391542702f6c3337a5a3334
rmit-s3542303-Raj-Adhikari <s3542303@student.rmit.edu.au>
2017-10-22 20:29:19 +1100

    Fixed scollbar hiding, Added auto scroll to bottom of chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -9,9 +9,8 @@
 
 </div>
 
-<div class="row posts" style="height: calc(100vh - 373px);overflow: scroll;">
-    <div class="col-md-12" style="overflow: scroll;">
-
+<div id="msg-wall" class="row" style="height: calc(100vh - 373px);overflow-y: scroll;overflow-x: hidden;">
+    <div class="col-md-12" style="overflow-y: scroll;overflow-x: hidden;">
     @foreach($posts as $post)
         @if($post->user_id == Auth::id())
             @include('layouts.message-sent')
@@ -19,25 +18,33 @@
             @include('layouts.message-rec')
         @endif
     @endforeach
-            </div>
+    </div>
 </div>
-<div class="" style="height: 200px;">
+
+<!-- Scroll to bottom of messages  -->
+<script>
+var objDiv = document.getElementById("msg-wall");
+objDiv.scrollTop = objDiv.scrollHeight;
+</script>
+
+
+<div class="row" style="height: 200px;">
+
+ <form action="{{ route('post.create')}}" method="post" style="height: 100%;margin-right: 10px;">
     <div class="col-md-10 col-xs-7" style="height: 100%;">
-        <form action="{{ route('post.create')}}" method="post" style="height: 100%;">
+       
             <div class="form-group" style="height: 100%;">
                 <input name="rec" value="{{$rid}}" hidden>
                 <textarea class="form-control" name="body" id="new-post" placeholder="enter your message" style="height: 100%; ">
                 </textarea>
             </div>
-
             <input type="hidden" value="{{Session::token()}}" name="_token">
-        </form>
     </div>
     <div class="col-md-2 col-xs-5" style="height: 100%">
         <button type="submit" class="btn btn-primary btn-block" style="height: 100%"> <h3><strong>Send</strong></h3></button>
     </div>
+       </form>
 </div>
-<br>
 <script>
     var token = '{{ Session::token() }}'
 </script>
